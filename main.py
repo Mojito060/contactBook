a5f74aacbe21b55ec174d2414c06b53685bdc582
Leander Wagner <lwagner@MacBookWagner.local>
2022-05-15 14:59:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+userinput1 = input("Was möchtest du machen?\n1: Kontakt suchen \n2: Neuen Kontakt erstellen")
+if userinput1 == "1":
+    print("Bitte gebe den Kontaktnamen ein:")
+
+elif userinput1 == "2":
+    print("Wie heißt der Kontakt?")
+else:
+    print("Ungültige eingabe")
+
